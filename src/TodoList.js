b1793a60d1791a5e5adeaf7853ba30640676db14
柳友仁 <lyr@liuyourendeMacBook-Pro.local>
2018-09-04 22:06:51 +0800

    update

---
+++ src/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+
+class TodoList extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div><input /><button>提交</button> </div>
+        <ul>
+          <li>xuexi</li>
+        </ul>
+      </Fragment>
+    );
+  }
+}
+
+export default TodoList;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
+
+// PWA progressive web applocation
+// https协议的服务器上
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
+// registerServiceWorker();
