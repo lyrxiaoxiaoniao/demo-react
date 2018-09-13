9346a5c2f3e00ad06a50c6fd430689ed5cff14fc
柳友仁 <lyr@liuyourendeMacBook-Pro.local>
2018-09-13 21:37:15 +0800

    add redux

--- src/TodoListRedux.js
+++ src/TodoListRedux.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Input,  List, Button } from 'antd';
 import { connect } from 'react-redux';
 import { getTitle } from './store/actions';
@@ -6,44 +6,32 @@ import TodoTitle from './components/TodoTitle';
 import * as actionTypes from './store/actionTypes';
 import './redux.css'
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props)
-    this.handleBtnClick = this.handleBtnClick.bind(this)
-  }
-  render() {
-    const { title, list, inputValue, onChangeValue } = this.props
+const TodoList = (props) => {
+    const { title, list, inputValue, onChangeValue, onBtnClick, onItemDelete } = props
+    const handelDelete = (index) => {
+      onItemDelete(index)
+    };
     return (
       <Fragment>
         <div className='app'>
           <TodoTitle title={title}></TodoTitle>
           <Input value={inputValue} onChange={onChangeValue} className='top' placeholder="todolist" />
-          <Button onClick={this.handleBtnClick}>提交</Button>
+          <Button onClick={onBtnClick}>提交</Button>
           <List
             className='toplist'
             bordered
             dataSource={list}
-            renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
+            renderItem={(item, index) => (<List.Item onClick={handelDelete.bind(this, index)}>{item}</List.Item>)}
           />
         </div>
       </Fragment>
     );
-  }
-  handleBtnClick() {
-    const { list, onBtnClick, inputValue } = this.props
-    onBtnClick([...list, inputValue])
-  }
-  handleItemDelete(index) {
-    const { list, onItemDelete } = this.props
-    list.splice(index, 1)
-    onItemDelete(list)
-  }
 }
 const mapStateToProps = (state) => {
   return {
     title: state.title,
     inputValue: state.inputValue,
-    list: state.list || []
+    list: state.list
   }
 }
 const mapDispatchToProps = (dispatch) => {
@@ -55,11 +43,11 @@ const mapDispatchToProps = (dispatch) => {
       const value = e.target.value
       dispatch({type: actionTypes.CHANGE_INPUTVALUE, inputValue: value})
     },
-    onBtnClick: (list) => {
-      dispatch({type: actionTypes.HANDLE_BTN_CLICK, list})
+    onBtnClick: () => {
+      dispatch({type: actionTypes.HANDLE_BTN_CLICK})
     },
-    onItemDelete: (list) => {
-      dispatch({type: actionTypes.HANDLE_CLICK_DELETE, list})
+    onItemDelete: (index) => {
+      dispatch({type: actionTypes.HANDLE_CLICK_DELETE, index})
     }
   }
 }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,24 +1,29 @@
 import * as actionTypes from './actionTypes'
-
-const todoReducer = (state={}, action) => {
+const defaultState = {
+  inputValue: '',
+  list: [],
+  title: ''
+}
+const todoReducer = (state = defaultState, action) => {
+  const newState = JSON.parse(JSON.stringify(state))
   switch (action.type) {
     case actionTypes.CHANGE_TITLE:
       return { ...state,
         title: action.title
       }
     case actionTypes.CHANGE_INPUTVALUE:
-      return { ...state,
+      return { ...newState,
         inputValue: action.inputValue
       }
     case actionTypes.HANDLE_BTN_CLICK:
-      return { ...state,
-        inputValue: '',
-        list: action.list
+      // const newState = JSON.parse(JSON.stringify(state))
+      newState.list.push(newState.inputValue)
+      return { ...newState,
+        inputValue: ''
       }
     case actionTypes.HANDLE_CLICK_DELETE:
-      console.log(state)
-      return { ...state,
-        list: action.list
+      newState.list.splice(action.index, 1)
+      return { ...newState
       }
     default:
       return state
